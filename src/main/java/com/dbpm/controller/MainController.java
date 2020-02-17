a5b89a45b4368a1ca9ea8db197b9a6a29a50e001
IrwinJuice <sevriukovmk@gmail.com>
2020-02-17 23:35:08 +0200

    rebuild

--- src/main/java/com/dbpm/controller/MainController.java
+++ src/main/java/com/dbpm/controller/MainController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 
 @Controller
 public class MainController {
